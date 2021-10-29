ffcf1439ca8f17c9ee0b0ab73945d4b38910f6cb
DavidRaulMiranda <78372708+DavidRaulMiranda@users.noreply.github.com>
2021-10-29 17:25:12 +0200

    commit
    desde

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Digimon> initialDigimons = []..add(Digimon('Gatomon'))..add(Digimon('Gomamon'))..add(Digimon('Leomon'));
+  List<Digimon> initialDigimons = [] /*..add(Digimon('Gatomon'))*/
+    ..add(Digimon('Gomamon'))
+     ..add(Digimon('Leomon'));
 
   Future _showNewDigimonForm() async {
-    Digimon newDigimon = await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+    Digimon newDigimon = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
       return AddDigimonFormPage();
     }));
     //print(newDigimon);
